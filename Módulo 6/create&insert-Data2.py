4cfb1b2dc8cb96e599a59d16388d546becb04154
Luca Zilio de Oliveira <lucaoliveira846@gmail.com>
2021-11-11 11:43:47 -0300

    Manipulating Inserts and generating graphics

--- Módulo 6/create&insert-Data2.py
+++ Módulo 6/create&insert-Data2.py
@@ -2,6 +2,7 @@ import random
 import sqlite3
 import datetime
 import time
+import matplotlib.pyplot as plt
 
 conn = sqlite3.connect('dsaa.db')
 c = conn.cursor()
@@ -20,12 +21,60 @@ def data_insert_var():
     c.execute("INSERT INTO produtos (date, prod_name, valor) VALUES (?, ?, ?)", (new_date, new_prod_name, new_valor))
     conn.commit()
     
-for i in range(10):
-    data_insert_var()
-    time.sleep(1)
+def leitura_todos_dados():
+    c.execute("SELECT * FROM PRODUTOS")
+    for linha in c.fetchall():
+        print(linha)
+        
+def leitura_registros():
+    c.execute("SELECT * FROM PRODUTOS WHERE valor > 60.0")
+    for linha in c.fetchall():
+        print(linha)
+        
+def leitura_colunas():
+    c.execute("SELECT * FROM PRODUTOS")
+    for linha in c.fetchall():
+        print(linha[3])
+        
+def atualiza_dados():
+    c.execute("UPDATE produtos SET valor = 70.0 WHERE valor = 82.0")    
+    conn.commit()
+
+def remove_dados():
+    c.execute("DELETE FROM produtos WHERE valor = 66.0")
+    conn.commit()
     
-c.execute('INSERT INTO produtos VALUES(68945, "2018-05-02 14:32:11", "Monitor", 100)')
-conn.commit()
+    
+def dados_grafico():
+    c.execute("SELECT id, valor FROM produtos")
+    ids = []
+    valores = []
+    dados = c.fetchall()
+    for linha in dados:
+        ids.append(linha[0])
+        valores.append(linha[1])
+    
+    plt.bar(ids, valores)
+    plt.show()    
+    
+#for i in range(10):
+#    data_insert_var()
+#    time.sleep(1)
+    
+#conn.commit()
+
+#leitura_todos_dados()
+#leitura_registros()
+#leitura_colunas()
+
+#leitura_todos_dados()
+#atualiza_dados()
+#leitura_todos_dados()
+#remove_dados()
+#leitura_todos_dados()
+
+dados_grafico()
 
 c.close()
 conn.close()
+
